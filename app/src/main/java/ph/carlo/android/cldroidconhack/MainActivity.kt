73d24cfb605afa221c826e1a780fdca111f2b3f0
Carlo Luis <carloluisbation@gmail.com>
2019-07-17 12:47:55 +0800

    Initial commit

---
+++ app/src/main/java/ph/carlo/android/cldroidconhack/MainActivity.kt
@@ -0,0 +1,12 @@
+mit package ph.carlo.android.cldroidconhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
